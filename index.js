cc5f9ab589f1348ac0156e2465f95f135c2e59fc
alex <alexchapman.ac2@gmail.com>
2018-06-29 15:51:38 +0100

    initial app

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+let version='v1.0.0';
+
+app.get('/', (req, res) => res.send('Hello World! ' + version));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
